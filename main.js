8ded10cb6b24a5f492487fb3151cb8fd2c3cc0fd
Gizem Şangür <gizem2002sangur@gmail.com>
2025-07-06 12:25:30 +0300

    Create study buddy and pomodoro screen

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow, screen } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const width = 300;
+  const height = 300;
+  const { width: screenW, height: screenH } = screen.getPrimaryDisplay().workAreaSize;
+
+  const win = new BrowserWindow({
+    width,
+    height,
+    x: screenW - width - 10,
+    y: screenH - height - 10,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    hasShadow: false,
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadFile(path.join(__dirname, "src/index.html"));
+}
+
+app.whenReady().then(createWindow);
